ff3c0cf101e53456ecfe727bbc1ff15f438b2738
Redcarlos <39527912+Redcarlos@users.noreply.github.com>
2021-04-27 18:16:04 -0500

    Inicializar repositorio
    hola inicializar repositorio

---
+++ datatTypes.js
@@ -0,0 +1,14 @@
+var myString = "This is a string";
+var myNumber = 1;
+var myBoolean = true;
+
+var myArray = [
+  [0, 2],
+  [1, 2],
+];
+
+var myObject = {
+  name: "Roman",
+  lastName: "Carrasco",
+  age: 2,
+};
---
+++ tarea.js
@@ -0,0 +1,36 @@
+const myMultiplication = (param1, param2) => {
+    let dato = param1 * param2;
+    return dato;
+}
+const param1 = 20;
+const param2 = 20;
+let dato = myMultiplication(param1,param2);
+//dato = dato + 20;
+//console.log(`La multiplicacion de: ${param1} * ${param2} = ${dato}`);
+//__________________________________________________________
+// array 
+let frutas = ["Manzana", "Banana","Naranja"];
+
+let primero = frutas[0];
+
+let ultimo = frutas[frutas.length - 1];
+//console.log(ultimo);
+
+frutas.forEach(function(elemento, indice, array){
+    //console.log(elemento, indice);
+    
+})
+//__________________________________________________________
+// objeto
+const pet = {
+    name:'pelusa',
+    type: 'perro',
+    age: 5
+}
+
+pet.name='luna'; //para modificar valor de un objeto
+pet.age='8';
+
+const {name, type, age} = pet;  //Destructuring 
+
+console.log(name, type, age);
